5b833b360774f09d32f3ec95f6118129e50700cf
IQ72 <105931001+raihanfaiq72@users.noreply.github.com>
2024-10-22 06:46:09 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MainScreen());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('hello aplikasi makanan'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kuliner Nusantara'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.blue),
+            onPressed: () {
+              // Tambahkan tindakan untuk pencarian di sini
+            },
+          ),
+        ],
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.blue),
+          onPressed: () {
+            // Tambahkan tindakan untuk menu di sini
+          },
         ),
+      ),
+      body: const Center(
+        child: Text('Hello aplikasi makanan'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // Tambahkan tindakan untuk tombol tambah di sini
+        },
       ),
     );
   }
